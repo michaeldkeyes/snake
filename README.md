7562aecdd3874c879e0511d43ceb867dc5ea6183
Mike <michael.d.keyes@gmail.com>
2020-10-08 15:10:28 -0400

    Local Storage High Score

---
+++ README.md
@@ -0,0 +1,3 @@
+# Snake
+
+Classic game of snake. Move around and eat as many food as you can without touching the walls or yourself! Keeps track of high score
---
+++ food.js
@@ -0,0 +1,40 @@
+import { onSnake, expandSnake } from "./snake.js";
+import { randomGridPosition } from "./grid.js";
+
+let food = getRandomFoodPosition();
+let score = 0;
+const EXPANSION_RATE = 2;
+const scoreSpan = document.getElementById("score");
+const highScoreSpan = document.getElementById("highScore");
+
+let highScore = localStorage.getItem("highScore");
+highScoreSpan.innerHTML = highScore;
+
+export function update() {
+  if (onSnake(food)) {
+    score += 5;
+    scoreSpan.innerHTML = score;
+    expandSnake(EXPANSION_RATE);
+    food = getRandomFoodPosition();
+  }
+
+  if (score > highScore) {
+    localStorage.setItem("highScore", score);
+  }
+}
+
+export function draw(gameBoard) {
+  const foodElement = document.createElement("div");
+  foodElement.style.gridRowStart = food.y;
+  foodElement.style.gridColumnStart = food.x;
+  foodElement.classList.add("food");
+  gameBoard.appendChild(foodElement);
+}
+
+function getRandomFoodPosition() {
+  let newFoodPosition;
+  while (newFoodPosition == null || onSnake(newFoodPosition)) {
+    newFoodPosition = randomGridPosition();
+  }
+  return newFoodPosition;
+}
